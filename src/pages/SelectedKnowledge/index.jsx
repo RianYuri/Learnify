3498e0ade82fe30fcea2476627b646350c174993
Rian Yuri <rianyurirgt@gmail.com>
2023-03-05 20:24:14 -0300

    feat: Screen SelectedKnowledge finish

--- src/pages/SelectedKnowledge/index.jsx
+++ src/pages/SelectedKnowledge/index.jsx
@@ -1,24 +1,81 @@
 import React from "react";
 //Import styled components
-import { ArticleQuestion, Category, FaceImg, Main, Section } from "./styled";
+import {
+  ArticleQuestions,
+  ArticleText,
+  CategoryTitle,
+  CategorySummary,
+  FaceImg,
+  Main,
+  Section,
+  Line,
+  Circles,
+  TypeQuestion,
+  Types,
+  ContainerCategories,
+  FigureCategory,
+  ImageCategory,
+  ButtonsContainer,
+  BackButton,
+  ArrowIcon,
+  StartTest,
+} from "./styled";
 
 // Import Images
-import FaceHappy from '../../assets/imgs/FaceDesktop-4.svg'
-
-
+import FaceHappy from "../../assets/imgs/FaceDesktop-4.svg";
+import LanguageLearning from "../../assets/imgs/CategoryImage/language-learning.svg";
+import ArrowLeft from "../../assets/imgs/arrow-left.svg";
+import ArrowRight from "../../assets/imgs/arrow-right.svg";
 
 // Codigo
-export const SelectedKnowledge = () => {
+const SelectedKnowledge = () => {
   return (
     <Main>
-      <FaceImg src={FaceHappy}/>
+      <FaceImg src={FaceHappy} />
       <Section>
-        <ArticleQuestion>
-        <Category>
-          Linguagens
-        </Category>
-        </ArticleQuestion>
+        {/* Primeira div pai */}
+
+        <ArticleText>
+          <CategoryTitle>Linguagens</CategoryTitle>
+          <CategorySummary>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+            interdum, odio et suscipit faucibus, sem orci rutrum mi.
+          </CategorySummary>
+        </ArticleText>
+
+
+        {/* Segunda div pai */}
+        <ContainerCategories>
+          <ArticleQuestions>
+            <Line>
+              <Circles></Circles>
+              <Circles></Circles>
+              <Circles></Circles>
+            </Line>
+            <Types>
+              <TypeQuestion>Português</TypeQuestion>
+              <TypeQuestion>Inglês</TypeQuestion>
+              <TypeQuestion>Espanhol</TypeQuestion>
+            </Types>
+          </ArticleQuestions>
+          <FigureCategory>
+            <ImageCategory src={LanguageLearning} />
+          </FigureCategory>
+        </ContainerCategories>
+
+
+        <ButtonsContainer>
+          <BackButton>
+            <ArrowIcon src={ArrowLeft} />
+            Voltar
+          </BackButton>
+          <StartTest>
+            Iniciar teste
+            <ArrowIcon src={ArrowRight} />
+          </StartTest>
+        </ButtonsContainer>
       </Section>
     </Main>
   );
 };
+export default SelectedKnowledge;
