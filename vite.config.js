d9603b522d4bf39be92964da5a3b6f5f4ba1a75a
Luiz Lopes <luizlopes12@outlook.com>
2023-03-07 19:50:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from 'vite-plugin-pwa'
-import reactRefresh from "@vitejs/plugin-react-refresh"
 
 // https://vitejs.dev/config/
 export default defineConfig({
