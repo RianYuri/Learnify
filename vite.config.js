c0916984e04e1a67e272a8a7f84cb95ba235bd9e
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-03-07 17:39:25 -0300

    feat: vite pwa config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,50 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+      VitePWA({  
+      registerType: 'prompt',  
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],  
+      manifest: {  
+  name: 'Learnify',  
+        short_name: 'Learnify',  
+        description: 'Seu app de quiz para aprender e se divertir',  
+        theme_color: '#ffffff',  
+        start_url: '/',  
+        icons: [  
+          {
+            src: "icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ],  
+      },  
+    }),
+  ],
+
   test: {
     globals: true,
     environment: "jsdom",
