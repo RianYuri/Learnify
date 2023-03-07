b23d2fd5957494fa058c41e45aac4db6f2acbef5
davitorress <dev.davitorres@gmail.com>
2023-03-07 02:03:49 -0300

    fix: rendering and props errors

--- src/pages/Questions/index.jsx
+++ src/pages/Questions/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 //Import Styles
 import { Main, SectionComponents, ArticleNext, NextPage, ArrowDisable, ProgressBarQuestions } from "./styled";
 // Import image
@@ -26,9 +26,11 @@ const Questions = () => {
 		QuestionId = QuestionId + 1;
 	};
 
-	if (questions.length === 10) {
-		navigate("/results");
-	}
+	useEffect(() => {
+		if (questions.length === 10) {
+			navigate("/results");
+		}
+	}, [questions.length]);
 
 	return (
 		<Main>
@@ -38,7 +40,13 @@ const Questions = () => {
 					<NextPage onClick={clickNextQuestions}>Próxima</NextPage>
 					<ArrowDisable src={ArrowRightDisable} />
 				</ArticleNext>
-				<ProgressBarQuestions type="range" min="0" max="100" value="0" ProgressValue={progress}></ProgressBarQuestions>
+				<ProgressBarQuestions
+					type="range"
+					min="0"
+					max="100"
+					defaultValue="0"
+					ProgressValue={progress}
+				></ProgressBarQuestions>
 			</SectionComponents>
 		</Main>
 	);
