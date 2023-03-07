d6942079c4b4dbf562113758d9a75321dad5b2d7
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-03-07 17:22:38 -0300

    Revert "fix: pwa config"
    This reverts commit 43011530c93651978058c002292120a26ea405d3.

--- vite.config.js
+++ vite.config.js
@@ -20,25 +20,25 @@ export default defineConfig({
         start_url: '/',  
         icons: [  
           {
-            src: "public/icons/manifest-icon-192.maskable.png",
+            src: "icons/manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any"
           },
           {
-            src: "public/icons/manifest-icon-192.maskable.png",
+            src: "icons/manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable"
           },
           {
-            src: "public/icons/manifest-icon-512.maskable.png",
+            src: "icons/manifest-icon-512.maskable.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any"
           },
           {
-            src: "public/icons/manifest-icon-512.maskable.png",
+            src: "icons/manifest-icon-512.maskable.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable"
