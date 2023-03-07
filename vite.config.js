43011530c93651978058c002292120a26ea405d3
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-03-07 16:53:41 -0300

    fix: pwa config

--- vite.config.js
+++ vite.config.js
@@ -20,25 +20,25 @@ export default defineConfig({
         start_url: '/',  
         icons: [  
           {
-            src: "icons/manifest-icon-192.maskable.png",
+            src: "public/icons/manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any"
           },
           {
-            src: "icons/manifest-icon-192.maskable.png",
+            src: "public/icons/manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable"
           },
           {
-            src: "icons/manifest-icon-512.maskable.png",
+            src: "public/icons/manifest-icon-512.maskable.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any"
           },
           {
-            src: "icons/manifest-icon-512.maskable.png",
+            src: "public/icons/manifest-icon-512.maskable.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable"
