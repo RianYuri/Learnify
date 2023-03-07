3774b93ff5bb74c680e72a0b945726589d55801b
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-03-07 17:21:57 -0300

    Revert "fix: pwa assets"
    This reverts commit 94062355b7e9547589e790d4019aafca88283762.

---
+++ serviceWorker.js
@@ -0,0 +1,38 @@
+//STORAGE OF BROWSER
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html"];
+const self = this;
+
+//installation
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+// actitivate the service worker
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)){
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+});
