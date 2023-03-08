1fa02e304258fb66ca675e86de3db593bd79030f
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-03-08 17:31:37 -0300

    Update index.jsx

--- src/pages/QuickStart/index.jsx
+++ src/pages/QuickStart/index.jsx
@@ -37,7 +37,7 @@ const QuickStart = () => {
 		});
 	};
 	const handleSaveUserName = async () => {
-		if(nameActive.name > 3){
+		if(nameActive.name >= 3){
 		try {
 			setUserData({ userName: nameActive.name });
 			localStorage.setItem("userName", nameActive.name);
